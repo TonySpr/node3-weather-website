01ac74562f70815fdc6e17766e9ec4413f54c6e4
Anthony Spradley <anthony_spradley@yahoo.com>
2020-05-12 16:34:34 -0400

    Code ready for Heroku

--- SRC/app.js
+++ SRC/app.js
@@ -9,6 +9,7 @@ console.log(path.join(__dirname, '../public'))
 
 //Set up handlebars engine config views location
 const app = express()
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 const publicDirectoryPath2 = path.join(__dirname, '../public/help.html')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -96,6 +97,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000!')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
